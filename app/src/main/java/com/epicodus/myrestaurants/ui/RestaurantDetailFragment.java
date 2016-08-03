24544fe9c019cca3d7caabe138460cb027b020a8
Summer Brochtrup <sbrochtrup@gmail.com>
2016-08-03 16:11:03 -0700

    move logout functionality nd change landscape detail layout to use icons instead of text

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailFragment.java
@@ -9,7 +9,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.view.LayoutInflater;
@@ -27,11 +29,8 @@ import android.widget.Toast;
 import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.models.Restaurant;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
@@ -58,28 +57,18 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
     private TextView mCategoriesLabel;
     private RatingBar mRatingBar;
     private TextView mWebsiteLabel;
+    private ImageView mWebsiteIcon;
     private TextView mPhoneLabel;
+    private ImageView mPhoneIcon;
     private TextView mAddressLabel;
-    private Button mSaveRestaurantButton;
+    private ImageView mAddressIcon;
+    private Button mBottomButton;
+    private FloatingActionButton mFAB;
+    private SupportMapFragment mMap;
     private Restaurant mRestaurant;
     private ArrayList<Restaurant> mRestaurants;
     private int mPosition;
     private String mSource;
-    private Toolbar mToolbar;
-    private CollapsingToolbarLayout collapsingToolbarLayout;
-
-    private SupportMapFragment mMap;
-
-    private void bindViews(View view) {
-        mImageLabel = (ImageView) view.findViewById(R.id.restaurantImageView);
-        mNameLabel = (TextView) view.findViewById(R.id.restaurantNameTextView);
-        mCategoriesLabel = (TextView) view.findViewById(R.id.categoryTextView);
-        mRatingBar = (RatingBar) view.findViewById(R.id.ratingBar);
-        mWebsiteLabel = (TextView) view.findViewById(R.id.websiteTextView);
-        mPhoneLabel = (TextView) view.findViewById(R.id.phoneTextView);
-        mAddressLabel = (TextView) view.findViewById(R.id.addressTextView);
-        mSaveRestaurantButton = (Button) view.findViewById(R.id.saveRestaurantButton);
-    }
 
     public static RestaurantDetailFragment newInstance(ArrayList<Restaurant> restaurants, Integer position, String source) {
         RestaurantDetailFragment restaurantDetailFragment = new RestaurantDetailFragment();
@@ -105,19 +94,8 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
         bindViews(view);
-
         mMap = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mMap.getMapAsync(this);
-
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            mToolbar = (Toolbar) view.findViewById(R.id.main_toolbar);
-            mToolbar.inflateMenu(R.menu.menu_main);
-            collapsingToolbarLayout = (CollapsingToolbarLayout) view.findViewById(R.id.main_collapsing);
-            collapsingToolbarLayout.setTitle(getResources().getString(R.string.app_name));
-            collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
-            collapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorPrimary));
-        }
-
         if (!mRestaurant.getImageUrl().contains(Constants.HTTP_FILTER)) {
             try {
                 Bitmap image = decodeFromFirebaseBase64(mRestaurant.getImageUrl());
@@ -132,39 +110,7 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
                     .centerCrop()
                     .into(mImageLabel);
         }
-
-        if (mSource.equals(Constants.SOURCE_SAVED)) {
-            mSaveRestaurantButton.setVisibility(View.GONE);
-        } else {
-            mSaveRestaurantButton.setOnClickListener(this);
-        }
-
-        mNameLabel.setText(mRestaurant.getName());
-        mCategoriesLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getCategories()));
-        mRatingBar.setRating((float)mRestaurant.getRating());
-        mPhoneLabel.setText(mRestaurant.getPhone());
-        mAddressLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getAddress()));
-        mWebsiteLabel.setOnClickListener(this);
-        mPhoneLabel.setOnClickListener(this);
-        mAddressLabel.setOnClickListener(this);
-
         return view;
-    }
-
-    public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
-        byte[] decodedByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
-        return BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        if (mSource.equals(Constants.SOURCE_SAVED)) {
-            inflater.inflate(R.menu.menu_photo, menu);
-        }
-        else {
-            inflater.inflate(R.menu.menu_main, menu);
-        }
     }
 
     @Override
@@ -172,17 +118,56 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
         switch (item.getItemId()) {
             case R.id.action_photo:
                 onLaunchCamera();
+                break;
+            case R.id.action_logout:
+                logout();
             default:
                 break;
         }
         return false;
     }
 
-    public void onLaunchCamera() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    @Override
+    public void onClick(View v) {
+        if (v == mWebsiteLabel || v == mWebsiteIcon) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(mRestaurant.getWebsite()));
+            startActivity(webIntent);
         }
+        if (v == mPhoneLabel || v == mPhoneIcon) {
+            Intent phoneIntent = new Intent(Intent.ACTION_DIAL,
+                    Uri.parse(String.format(getResources().getString(R.string.tel_format),
+                            mRestaurant.getPhone())));
+            startActivity(phoneIntent);
+        }
+        if (v == mAddressLabel || v == mAddressIcon) {
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(String.format(getResources().getString(R.string.map_format),
+                            Double.toString(mRestaurant.getLatitude()),
+                            Double.toString(mRestaurant.getLongitude()),
+                            mRestaurant.getName())));
+            startActivity(mapIntent);
+        }
+        if (mSource.equals(Constants.SOURCE_SAVED)) {
+            if (v == mBottomButton || v == mFAB) {
+                onLaunchCamera();
+            }
+        } else {
+            if (v == mBottomButton || v == mFAB) {
+                saveRestaurantToFirebase();
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap map = googleMap;
+        LatLng restaurantMarker = new LatLng(mRestaurant.getLatitude(), mRestaurant.getLongitude());
+        map.addMarker(new MarkerOptions().position(restaurantMarker).title(mRestaurant.getName()));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(restaurantMarker, 15));
+        UiSettings uiSettings = map.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
     }
 
     @Override
@@ -192,6 +177,77 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
             Bitmap imageBitmap = (Bitmap) extras.get(Constants.BITMAP_EXTRA);
             mImageLabel.setImageBitmap(imageBitmap);
             encodeBitmapAndSaveToFirebase(imageBitmap);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        if (mSource.equals(Constants.SOURCE_SAVED)) {
+            inflater.inflate(R.menu.menu_photo, menu);
+        } else {
+            inflater.inflate(R.menu.menu_main, menu);
+        }
+    }
+
+    private void bindViews(View view) {
+        mImageLabel = (ImageView) view.findViewById(R.id.restaurantImageView);
+        mNameLabel = (TextView) view.findViewById(R.id.restaurantNameTextView);
+        mCategoriesLabel = (TextView) view.findViewById(R.id.categoryTextView);
+        mRatingBar = (RatingBar) view.findViewById(R.id.ratingBar);
+        mFAB = (FloatingActionButton) view.findViewById(R.id.fab);
+        mFAB.setOnClickListener(this);
+        mNameLabel.setText(mRestaurant.getName());
+        mCategoriesLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getCategories()));
+        mRatingBar.setRating((float)mRestaurant.getRating());
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mWebsiteLabel = (TextView) view.findViewById(R.id.websiteTextView);
+            mPhoneLabel = (TextView) view.findViewById(R.id.phoneTextView);
+            mAddressLabel = (TextView) view.findViewById(R.id.addressTextView);
+            mBottomButton = (Button) view.findViewById(R.id.saveRestaurantButton);
+            mBottomButton.setOnClickListener(this);
+            mPhoneLabel.setOnClickListener(this);
+            mAddressLabel.setOnClickListener(this);
+            Toolbar toolbar = (Toolbar) view.findViewById(R.id.main_toolbar);
+            ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+            setHasOptionsMenu(true);
+            mAddressLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getAddress()));
+            CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) view.findViewById(R.id.main_collapsing);
+            collapsingToolbarLayout.setTitle(getResources().getString(R.string.app_name));
+            collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+            collapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorPrimary));
+        } else {
+            mWebsiteIcon = (ImageView) view.findViewById(R.id.websiteIcon);
+            mPhoneIcon = (ImageView) view.findViewById(R.id.phoneIcon);
+            mAddressIcon = (ImageView) view.findViewById(R.id.addressIcon);
+            mWebsiteIcon.setOnClickListener(this);
+            mPhoneIcon.setOnClickListener(this);
+            mAddressIcon.setOnClickListener(this);
+        }
+        if (mSource.equals(Constants.SOURCE_SAVED)) {
+            mFAB.setImageResource(R.drawable.ic_camera_alt_white_24dp);
+            mBottomButton.setText(getResources().getString(R.string.photo_button));
+        }
+    }
+
+    private void saveRestaurantToFirebase() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+        DatabaseReference restaurantRef = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
+                .child(uid);
+        DatabaseReference pushRef = restaurantRef.push();
+        String pushId = pushRef.getKey();
+        mRestaurant.setPushId(pushId);
+        pushRef.setValue(mRestaurant);
+        Toast.makeText(getContext(), getResources().getString(R.string.saved_toast), Toast.LENGTH_SHORT).show();
+    }
+
+    public void onLaunchCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
@@ -207,54 +263,16 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
         ref.setValue(imageEncoded);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v == mWebsiteLabel) {
-            Intent webIntent = new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(mRestaurant.getWebsite()));
-            startActivity(webIntent);
-        }
-
-        if (v == mPhoneLabel) {
-            Intent phoneIntent = new Intent(Intent.ACTION_DIAL,
-                    Uri.parse(String.format(getResources().getString(R.string.tel_format),
-                            mRestaurant.getPhone())));
-            startActivity(phoneIntent);
-        }
-
-        if (v == mAddressLabel) {
-            Intent mapIntent = new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(String.format(getResources().getString(R.string.map_format),
-                            Double.toString(mRestaurant.getLatitude()),
-                            Double.toString(mRestaurant.getLongitude()),
-                            mRestaurant.getName())));
-            startActivity(mapIntent);
-        }
-
-        if (v == mSaveRestaurantButton) {
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            String uid = user.getUid();
-            DatabaseReference restaurantRef = FirebaseDatabase
-                    .getInstance()
-                    .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
-                    .child(uid);
-            DatabaseReference pushRef = restaurantRef.push();
-            String pushId = pushRef.getKey();
-            mRestaurant.setPushId(pushId);
-            pushRef.setValue(mRestaurant);
-            Toast.makeText(getContext(), getResources().getString(R.string.saved_toast), Toast.LENGTH_SHORT).show();
-        }
+    public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
+        byte[] decodedByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        GoogleMap map = googleMap;
-        LatLng restaurantMarker = new LatLng(mRestaurant.getLatitude(), mRestaurant.getLongitude());
-        map.addMarker(new MarkerOptions().position(restaurantMarker).title(mRestaurant.getName()));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(restaurantMarker, 15));
-        UiSettings uiSettings = map.getUiSettings();
-        uiSettings.setZoomControlsEnabled(true);
-        uiSettings.setCompassEnabled(true);
-        uiSettings.setMyLocationButtonEnabled(true);
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
