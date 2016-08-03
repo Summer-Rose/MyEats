aad7a8e607b4e1aceabe3eea5505105653a43950
Summer Brochtrup <sbrochtrup@gmail.com>
2016-08-03 16:25:30 -0700

    clean messy code: reorganize methods, fix spacing and indentation, remove unused imports

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
