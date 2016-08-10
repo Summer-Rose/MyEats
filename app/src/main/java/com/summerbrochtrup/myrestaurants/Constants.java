cd1a6ac69a1d4f22b6cbce49e07236ba8c59d688
Summer Brochtrup <sbrochtrup@gmail.com>
2016-08-09 18:43:32 -0700

    remove constants that are no longer  needed

--- app/src/main/java/com/summerbrochtrup/myrestaurants/Constants.java
+++ app/src/main/java/com/summerbrochtrup/myrestaurants/Constants.java
@@ -17,27 +17,17 @@ public class Constants {
     public static final String SERIALIZED_NAME_IMAGE_URL = "image_url";
     public static final String SERIALIZED_NAME_ID = "id";
 
-    /* Firebase Constants*/
-    public static final String FIREBASE_CHILD_RESTAURANTS = "restaurants";
-    public static final String FIREBASE_QUERY_INDEX = "index";
-    public static final String FIREBASE_CHILD_IMAGEURL = "imageUrl";
-
     /* Shared Preferences Constants */
     public static final String PREFERENCES_LOCATION_KEY = "location";
-    public static final String EXTRA_KEY_POSITION = "position";
-    public static final String EXTRA_KEY_RESTAURANTS = "restaurants";
 
     /* Intent Extra Keys */
-    public static final String KEY_SOURCE = "source";
+    public static final String EXTRA_KEY_RESTAURANT = "restaurant";
+    public static final String EXTRA_KEY_SOURCE = "source";
+
+    /* Source Keys */
     public static final String SOURCE_SAVED = "saved";
     public static final String SOURCE_FIND = "find";
 
-    /* Fonts */
-    public static final String EMPTY_STRING = "";
-    public static final String FONT_OSTRICH_REGULAR = "fonts/ostrich-regular.ttf";
-
     /* Extras */
-    public static final String HTTP_FILTER = "http";
-    public static final String BITMAP_EXTRA = "data";
-    public static final String INDEX_NOT_SPECIFIED = "not_specified";
+    public static final String EMPTY_STRING = "";
 }
